fdd857b4291b60f764fd509ea5daec4e79a43469
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-16 22:01:51 +0530

    Added:Program for Contact Number Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,17 +5,12 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public boolean validateEmail(String Email) {
-        String regex = "^[a-zA-Z0-9+_.]+@[a-zA-Z.-]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(Email);
+    public boolean validateContactNumber(String contactNumber) {
 
-        if (matcher.matches()) {
-            System.out.println("Valid Email");
-
-        } else {
-            System.out.println("Invalid Email....!");
-        }
-        return false;
+        String Regex = "^[0-9]{1}[0-9]{1}\\s{0,1}[7-9]{1}[0-9]{9}";
+        Pattern pattern = Pattern.compile(Regex);
+        Matcher matcher = pattern.matcher(contactNumber);
+        return matcher.matches();
     }
+
 }
--- UserRegistrationTestCases.java
+++ UserRegistrationTestCases.java
@@ -8,16 +8,37 @@ public class UserRegistrationTestCases {
     UserRegistration userRegistration = new UserRegistration();
 
     @Test
-    public void whenGivenEmailWithSpecialCharactersAtProperPlaces_shouldReturnTrue() {
-
-        boolean validationStatus = userRegistration.validateEmail("chaudharitoshita@gmail.com");
+    public void whenGivenContactWithCountryCodeAnd10Digits_shouldReturnTrue() {
+        boolean validationStatus = userRegistration.validateContactNumber("91 9603165488");
         Assert.assertEquals(true, validationStatus);
     }
 
     @Test
-    public void whenGivenEmailWithSpecialCharactersNotAtProperPlaces_shouldReturnFalse() {
-        boolean validationStatus = userRegistration.validateEmail("chaudhari123gmailcom");
+    public void whenGivenContactWithoutCountryCodeAndLessThan10Digits_shouldReturnFalse() {
+
+        boolean validationStatus = userRegistration.validateContactNumber("9603165488");
         Assert.assertEquals(false, validationStatus);
     }
 
-}
+    @Test
+    public void whenGivenContactWithCountryCodeAndLessThan10Digits_shouldReturnFalse() {
+
+        boolean validationStatus = userRegistration.validateContactNumber(" 91 9603165488");
+        Assert.assertEquals(false, validationStatus);
+    }
+
+    @Test
+    public void whenGivenContactWithoutCountryCodeAndWith10Digits_shouldReturnFalse() {
+
+        boolean validationStatus = userRegistration.validateContactNumber("9603165488");
+        Assert.assertEquals(false, validationStatus);
+    }
+
+    @Test
+    public void whenGivenContactWithAlphabetsAndSpecialCharacters_shouldReturnFalse() {
+
+        boolean validationStatus = userRegistration.validateContactNumber("960%5488");
+        Assert.assertEquals(false, validationStatus);
+    }
+
+} 
