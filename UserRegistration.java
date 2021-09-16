91fd8eb92d0a802cca5c12580ef44253f76c0ed3
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-16 23:16:18 +0530

    Added:Program for password rule validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,16 +1,30 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public boolean validateContactNumber(String contactNumber) {
+    public static void validatePassword(String password) {
+        String passwordPattern = "^[a-zA-Z0-9@#!]{8}[a-zA-z0-9@#!]*";
+        Pattern pattern = Pattern.compile(passwordPattern);
+        Matcher matcher = pattern.matcher(password);
 
-        String Regex = "^[0-9]{1}[0-9]{1}\\s{0,1}[7-9]{1}[0-9]{9}";
-        Pattern pattern = Pattern.compile(Regex);
-        Matcher matcher = pattern.matcher(contactNumber);
-        return matcher.matches();
+        if (matcher.find()) {
+            System.out.println(password + " is valid");
+        } else {
+            System.out.println(password + " is not valid");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String password = "";
+        System.out.println("Enter the password");
+        password = sc.nextLine();
+        validatePassword(password);//validation of Password
+        sc.close();
     }
 
 }
